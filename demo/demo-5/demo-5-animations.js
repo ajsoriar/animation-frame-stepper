f94f13e3dc2fb28c62ddd46f4ac905af8fa92da6
asr <ajsoriar@gmail.com>
2018-01-07 14:53:11 +0100

    example 5

---
+++ demo/demo-5/demo-5-animations.js
@@ -0,0 +1,72 @@
+/*
+
+// ------------------------------
+// define animation 3 properties
+// ------------------------------
+
+AS.attachAnimation({
+    id: null,
+    name: "Animation-1",
+    label: null,
+    description: "Animation Object Description",
+    duration: null,
+    stepStartTime: null,
+    stepsNum: 30,
+    milisecondsStep: 100,
+    elapsedTime: 0,
+    repeatForever: false,
+    repeatTimes: 0,
+    currentStep: null,
+    func: function( _animation ) {
+        //console.log("Andres animation function! _animation:", _animation );
+        window.domEl_3.css('margin-left', ( _animation.currentStep * 20 ) +"px");
+    },
+    stepsFuncs: [],
+    autodestroy: true, // Automatically removes the animation object and functions when the animation finishes
+    autoStart: false,
+    onStart: function() { 
+        //console.log( "Andres amimation starts now!");
+        window.domEl_3 = $('#animated-3');
+    },
+    whenFinish: function() { 
+        //console.log( "Andres amimation ends now!");
+    },
+    status: 0, // (0) Stopped, (1) On going.
+})
+
+// ------------------------------
+// define animation 4 properties
+// ------------------------------
+
+AS.attachAnimation({
+    label: "yellow",
+    stepsNum: 30,
+    milisecondsStep: 500,
+    func: function( _animation ) {
+        //console.log("Andres animation function! _animation:", _animation );
+        window.domEl_4.css('margin-left', ( _animation.currentStep * 20 ) +"px");
+    },
+    onStart: function() { 
+        //console.log( "Andres amimation starts now!");
+        window.domEl_4 = $('#animated-4');
+    }
+})
+
+// ------------------------------
+// define animation 5 properties
+// ------------------------------
+
+AS.attachAnimation({
+    stepsNum: 40,
+    milisecondsStep: 10,
+    func: function( _animation ) {
+        //console.log("Andres animation function! _animation:", _animation );
+        window.domEl_5.css('margin-left', ( _animation.currentStep * 10 ) +"px");
+    },
+    onStart: function() { 
+        //console.log( "Andres amimation starts now!");
+        window.domEl_5 = $('#animated-5');
+    }
+})
+
+*/
---
+++ demo/demo-5/demo-5.js
@@ -0,0 +1,40 @@
+// ----------------------------------------------------------------------
+// myStuff.js
+// ----------------------------------------------------------------------
+
+AS.init();
+
+function start() {
+    //animationStartTime = window.performance.now();
+    //requestId = window.requestAnimationFrame(animate);
+    AS.startByName("Animation-1");
+}
+
+function stop() {
+    //if (requestId) window.cancelAnimationFrame(requestId);
+    //requestId = 0;
+    AS.stopByName("Animation-1");
+}
+
+function stopByLabel() {
+    //if (requestId) window.cancelAnimationFrame(requestId);
+    //requestId = 0;
+    AS.stopByLabel("clouds");
+}
+
+function pause() {
+    //if (requestId) window.cancelAnimationFrame(requestId);
+    //requestId = 0;
+    AS.pauseByName("Animation-1");
+}
+
+function gotoStep() {
+    //if (requestId) window.cancelAnimationFrame(requestId);
+    //requestId = 0;
+    AS.goToStepByName("Animation-1", 3);
+}
+
+function resetByLabel( lbl ) {
+    AS.resetAnimationByLabel('yellow');
+}
+
