f3d3abdee51a7e070773d517ec2fc1c496cbe343
asr <ajsoriar@gmail.com>
2018-01-09 00:58:48 +0100

    some changes

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+//var webpack = require('webpack');
+
+const webpack = require("webpack");
+
+module.exports = {
+  entry: {
+    "bundle": "./entry.js",
+    "bundle.min": "./entry.js",
+  },
+  devtool: "source-map",
+  output: {
+    path: "./dist",
+    filename: "[name].js"
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      include: /\.min\.js$/,
+      minimize: true
+    })
+  ]
+};
